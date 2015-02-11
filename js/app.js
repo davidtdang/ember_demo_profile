6fc63be979fecfbe2e1b1f24019985e99c404250
David Dang <dtudang@gmail.com>
2015-02-11 15:01:08 -0800

    fixed routing and added model

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  this.route('about');
-  this.route('portfolio');
-  this.route('resume');});
-
-// App.IndexRoute = Ember.Route.extend({
-//   model: function() {
-//     return ['red', 'yellow', 'blue'];
-//   }
-// });
+window.App = Ember.Application.create();
--- js/router.js
+++ js/router.js
@@ -1,5 +1,19 @@
 App.Router.map(function(){
-  this.route('about');
-  this.route('portfolio');
-  this.route('resume');
+  this.route("about");
+  this.route("resume");
+  this.route("portfolio");
+  this.route("project", {path: "/portfolio/:project_id"});
 });
+
+
+App.PortfolioRoute = Ember.Route.extend({
+  model: function(){
+    return [
+      {id: 1, title:"Taught Luke how to do shit"},
+      {id: 2, title:"Got comically overpowered"},
+      {id: 3, title:"Became left handed for awhile"},
+      {id: 4, title:"Founded Murrica"},
+      {id: 5, title:"Made some pureass rock candy"},
+    ]
+  }
+})
