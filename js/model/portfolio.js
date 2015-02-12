11a5eadbdfdcde00fa010e29c6c2e91485802d71
David Dang <dtudang@gmail.com>
2015-02-11 18:02:59 -0800

    added bind attr images

--- js/model/portfolio.js
+++ js/model/portfolio.js
@@ -4,9 +4,9 @@ App.Project = DS.Model.extend({
 });
 
 App.Project.FIXTURES = [
-    {id: 1, title:"Taught Luke how to do shit", image: "jedi.jpg"},
-    {id: 2, title:"Got comically overpowered",image: "wolverine.jpg"},
-    {id: 3, title:"Became left handed for awhile", image: "link.jpg"},
-    {id: 4, title:"Founded Murrica", image: "president.jpg"},
-    {id: 5, title:"Made some pureass rock candy", image: "walter.jpg"},
+    {id: 1, title:"Taught Luke how to do shit", image: "images/jedi.jpg"},
+    {id: 2, title:"Got comically overpowered",image: "images/wolverine.jpg"},
+    {id: 3, title:"Became left handed for awhile", image: "images/link.jpg"},
+    {id: 4, title:"Founded Murrica", image: "images/president.jpg"},
+    {id: 5, title:"Made some pureass rock candy", image: "images/walter.jpg"},
   ]
